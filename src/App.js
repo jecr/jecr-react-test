a20375a8efa7de1cdb35d3dc3d6a55f6bf1579a3
Jorge Cortés <jorge.ecro@gmail.com>
2020-11-27 17:15:08 -0600

    Initializing repo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,92 @@
-import logo from './logo.svg';
 import './App.css';
+import { getStates } from './fakeAPI';
+import { useEffect, useState } from 'react';
+import styled from 'styled-components';
+
+const MainContainer = styled.div`
+  max-width: 50%;
+  margin: 1rem auto;
+  text-align: center;
+`;
+
+const SearchContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  input {
+    width: 100%;
+  }
+`;
+
+const StatesList = styled.ul`
+  list-style: none;
+  padding: 0;
+  li {
+    padding: 1rem;
+    border: 1px solid #ccc;
+  }
+`;
+
+const TextInput = styled.input`
+  border: none;
+  border-bottom: 1px solid #ccc;
+  padding: .5rem;
+  box-sizing: border-box;
+  margin-bottom: .5rem;
+`;
+
+const SearchButton = styled.button`
+  background: #eee;
+  border: none;
+  cursor: pointer;
+  padding: .4rem;
+  &:hover {
+    background: #444;
+    color: white;
+  }
+`;
 
 function App() {
+  const [usrQuery, setUsrQuery] = useState('');
+  const [countryStates, setCountryStates] = useState([]);
+  const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    getStates().then(data => setCountryStates(data));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <MainContainer>
+      <SearchContainer>
+        <TextInput
+          type="text"
+          value={usrQuery}
+          placeholder="Please enter your query"
+          onChange={e => setUsrQuery(e.target.value)}
+        />
+        <SearchButton
+          onClick={() => setFilter(usrQuery)}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          Search
+      </SearchButton>
+      </SearchContainer>
+      <StatesList>
+        {
+          countryStates.length > 0 ?
+            countryStates
+              .filter(entry =>
+                entry.name.toLowerCase().indexOf(filter.toLowerCase()) > -1 && entry
+              )
+              .map(entry =>
+                <li
+                  key={`country-state-${entry.id}`}
+                >
+                  {entry.name}
+                </li>
+              ) :
+            "Loading states..."
+        }
+      </StatesList>
+    </MainContainer>
   );
 }
 
